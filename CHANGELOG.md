1461608ba226366077b4286e9a31da31c5080262
core-cookbooks <nagalakshmi.n@cloudenablers.com>
2015-04-20 19:03:15 +0530

    Initial Release

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+windows Cookbook CHANGELOG
+=======================
+This file is used to list changes made in each version of the windows cookbook.
+
+v1.34.8 (2015-10-31)
+--------------------
+- initial release
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'windows'
+maintainer       'Cloudenablers'
+maintainer_email 'nagalakshmi.n@cloudenablers.com'
+license          'Apache 2.0'
+description      'Provides a set of useful Windows-specific primitives.'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '1.34.8'
+supports         'windows'
+depends          'chef_handler'
